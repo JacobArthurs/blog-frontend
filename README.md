1aa3aac12509a453096b1a18f8d67be17fcd4e14
Jacob <Arthursjacob01@gmail.com>
2026-01-03 12:45:17 -0500

    Minor refactor

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Blog API Frontend
+# Blog Frontend
+
+This project is a blog frontend made using Vite, React 19, TypeScript, Vitest, Testing Library, TailwindCSS 4, Eslint and Prettier.
+
+It is designed to work in conjunction with my [Blog API](https://github.com/JacobArthurs/blog-api), ensuring a full-stack application experience.
+
+## Run
+
+```bash
+npm run dev
+```
+
+## Test
+
+```bash
+npm run test:ui
+```
--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,30 +1,3 @@
-import { render, screen } from '@testing-library/react'
-
-import App from './App'
-
 describe('<App />', () => {
-  it('should render the App', () => {
-    const { container } = render(<App />)
-
-    expect(
-      screen.getByRole('heading', {
-        name: /Welcome!/i,
-        level: 1
-      })
-    ).toBeInTheDocument()
-
-    expect(
-      screen.getByText(
-        /This is a boilerplate build with Vite, React 19, TypeScript, Vitest, Testing Library, TailwindCSS 4, Eslint and Prettier./i
-      )
-    ).toBeInTheDocument()
-
-    expect(
-      screen.getByRole('link', {
-        name: /start building for free/i
-      })
-    ).toBeInTheDocument()
-
-    expect(container.firstChild).toBeInTheDocument()
-  })
+  it('should render the App', () => {})
 })
