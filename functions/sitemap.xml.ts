cf249d2f3cfeba7ba0fbeca43eb9093e0eee19ba
Jacob <Arthursjacob01@gmail.com>
2026-01-15 21:56:45 -0500

    Add sitemap function

---
+++ functions/sitemap.xml.ts
@@ -0,0 +1,8 @@
+export async function onRequest(context: {
+  env: { VITE_API_BASE_URL: string }
+}) {
+  const res = await fetch(`${context.env.VITE_API_BASE_URL}/blog/sitemap.xml`)
+  return new Response(await res.text(), {
+    headers: { 'Content-Type': 'application/xml' }
+  })
+}
