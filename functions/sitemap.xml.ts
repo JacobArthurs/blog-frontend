adcc05188ab2e01c8ed410db7e2a3be43a5263e1
Jacob <Arthursjacob01@gmail.com>
2026-01-15 21:58:23 -0500

    Fix blog api url

--- functions/sitemap.xml.ts
+++ functions/sitemap.xml.ts
@@ -1,7 +1,7 @@
 export async function onRequest(context: {
   env: { VITE_API_BASE_URL: string }
 }) {
-  const res = await fetch(`${context.env.VITE_API_BASE_URL}/blog/sitemap.xml`)
+  const res = await fetch(`${context.env.VITE_API_BASE_URL}/sitemap.xml`)
   return new Response(await res.text(), {
     headers: { 'Content-Type': 'application/xml' }
   })
